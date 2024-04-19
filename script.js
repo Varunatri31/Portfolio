22347a5c85343ea9b8047b83fb9290fc6b927107
Varun Atri <19309785-VarunAtri@users.noreply.replit.com>
2024-04-19 14:38:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings: ["Coder", "Web Designer", "Editor", "Creator"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+});
+                      
